074628e9dd73329b30df40807eb45c2809808b30
Henry Wallis <henry.wallis@metaswitch.com>
2021-04-25 20:11:47 +0100

    Simplify extension logic

--- move.js
+++ move.js
@@ -1,47 +1,14 @@
-// Shout out to Mohnish Thallavajhula, whose extension 'Rearrange tabs' is the basis
-// of this extension.
-
-chrome.commands.onCommand.addListener(function(command) {
-
-    var shift = ('previous-tab' == command) ? -1 : 1;
-
-    var shiftTab = function(tabs) {
-        // This function expects to receive all tabs in the current window
-
-        // Get number of tabs
-        var tabCount = tabs.length
-
-        // Get active tab
-        for (var i = 0, len = tabCount; i < len; i++){
-            if (tabs[i].active) {
-                var activeTab = tabs[i]
-            }
-        }
-
-        // Calculate index of target tab
-        var targetIndex = 0
-
-        if (activeTab.index + shift < 0) {
-            // If we're on the leftmost tab and move left, move to the end.
-            targetIndex = tabCount - 1
-
-        } else if (activeTab.index + shift > tabCount - 1) {
-            // Similarly if we're on the rightmost tab and move right, move to the beginning.
-            targetIndex = 0
-
-        } else {
-            // Otherwise move left or right as commanded.
-            targetIndex = activeTab.index + shift
-        }
-
-        // Get ID of target tab
-        var targetID = tabs[targetIndex].id
-
-        // Set target tab as active
-        chrome.tabs.update(targetID, {active : true});
-    };
-
-    chrome.tabs.query({
-        currentWindow: true,
-    }, shiftTab);
+chrome.commands.onCommand.addListener((command) => {
+  const changeInTabIndex = command === "previous-tab" ? -1 : 1;
+  chrome.tabs.query({ currentWindow: true }, changeTabIndex(changeInTabIndex));
 });
+
+const changeTabIndex = (delta) => (tabs) => {
+  const currentTabIndex = tabs.find((tab) => tab.active).index;
+
+  // Add tab length before using % to always get positive modulus
+  const targetTabIndex = (currentTabIndex + delta + tabs.length) % tabs.length;
+  const targetTabId = tabs[targetTabIndex].id;
+
+  chrome.tabs.update(targetTabId, { active: true });
+};
